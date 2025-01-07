1f67a282621888726c88b2a07fdb5318b2c9b55d
Sambit Mondal <sambitmondal2005@gmail.com>
2025-01-07 22:05:24 +0530

    Policies page completed

---
+++ src/app/policies/page.js
@@ -0,0 +1,48 @@
+import { ArrowRightCircleIcon } from '@heroicons/react/24/solid'
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='w-full min-h-screen relative'>
+      <Image
+        src="/Bg-screen.jpg"
+        alt="Background image"
+        width={1000}
+        height={1000}
+        priority
+        className='fixed top-0 h-screen w-full z-[-2]'
+      />
+      <div className='absolute inset-0 bg-[#000000] opacity-70 z-[-1]'></div>
+      <div className='w-full px-20 pt-28 z-20 pb-5 gap-2'>
+        <p className='font-carter tracking-wider text-4xl drop-shadow-text text-white'>
+          POLICIES
+        </p>
+        <hr className='border-none h-[1px] bg-white w-full' />
+      </div>
+      <div className='px-20 w-full h-full flex items-center justify-center flex-col gap-5'>
+        <div className='rounded-full border-2 border-green-500 w-full py-2 text-white font-bold px-5 text-lg flex items-center justify-between bg-black'>
+          Return and Refund Policy
+          <Link href='https://www.freeprivacypolicy.com/live/60302485-04c9-4815-9bcd-18fde21970dd' target='_blank'>
+            <ArrowRightCircleIcon className='size-10 text-green-500 transition duration-200 ease-in-out hover:scale-125' />
+          </Link>
+        </div>
+        <div className='rounded-full border-2 border-green-500 w-full py-2 text-white font-bold px-5 text-lg flex items-center justify-between bg-black'>
+          Privacy Policy
+          <Link href='https://www.freeprivacypolicy.com/live/3ee16d22-dfa1-4b0b-9dd6-1f5cf63a44eb' target='_blank'>
+            <ArrowRightCircleIcon className='size-10 text-green-500 transition duration-200 ease-in-out hover:scale-125' />
+          </Link>
+        </div>
+        <div className='rounded-full border-2 border-green-500 w-full py-2 text-white font-bold px-5 text-lg flex items-center justify-between bg-black'>
+          Disclaimer Policy
+          <Link href='https://www.freeprivacypolicy.com/live/2e74183c-5805-492f-855b-483de1306bb1'>
+            <ArrowRightCircleIcon className='size-10 text-green-500 transition duration-200 ease-in-out hover:scale-125' />
+          </Link>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default About
--- src/components/Navbar/NavbarMain.jsx
+++ src/components/Navbar/NavbarMain.jsx
@@ -16,13 +16,13 @@ const NavbarMain = () => {
     const tabMapping = useMemo(() => ({
         '/': 'home',
         '/artworks': 'artworks',
-        '/about': 'about',
+        '/policies': 'policies',
         '/contact': 'contact',
     }), []);
 
     useEffect(() => {
         const activeTab = Object.keys(tabMapping)
-            .sort((a, b) => b.length - a.length)  // Sort paths by length in descending order
+            .sort((a, b) => b.length - a.length)
             .find(path => pathname.startsWith(path)) || '';
         setActiveTab(tabMapping[activeTab]);
     }, [pathname, tabMapping]);
@@ -41,7 +41,7 @@ const NavbarMain = () => {
     return (
         <div className="border-2 border-green-500 shadow-md shadow-green-400 drop-shadow-2xl w-[70%] py-2 rounded-full bg-black flex items-center justify-around">
             <NavItem icon={HomeIcon} tabName="home" path="/" />
-            <NavItem icon={BookOpenIcon} tabName="about" path="/about" />
+            <NavItem icon={BookOpenIcon} tabName="policies" path="/policies" />
             <NavItem icon={PaintBrushIcon} tabName="artworks" path="/artworks" />
             <NavItem icon={PhoneIcon} tabName="contact" path="/contact" />
         </div>
