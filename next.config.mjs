37596ddbc45a9e7f6df0d6da279175f425e5fa94
Sambit Mondal <sambitmondal2005@gmail.com>
2025-01-09 00:57:32 +0530

    Updated signup route

--- next.config.mjs
+++ next.config.mjs
@@ -10,7 +10,7 @@ const nextConfig = {
         ],
     },
     experimental: {
-        outputFileTracingExcludes: { '*': ['**/api/**'] },
+        outputFileTracingExcludes: { '*': ['**/app/api/**'] }, // Only if needed
     },
 };
 
